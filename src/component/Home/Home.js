9eb597bbfb01cb0cc637c708569e6c5c644afcef
SouravBiswas238 <souravbiswas427@gmail.com>
2022-11-15 19:43:57 +0600

    first commit

---
+++ src/component/Home/Home.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className=' px-[74px] pt-[37px] w-[1366px] m-auto  bg-[#FFFFFF] text-[#000000]'>
+
+            <div className='pt-[36px] px-[187px]'>
+                <h4 className=''>   UI/UX > Refer & Earn > Friends Referred</h4>
+                <div>
+                    <h4 className='text-[#800080] text-[16px] pt-[27px] mb-[18px]'>Your Referral Code</h4>
+                    <button className='my-button p-3 text-center m-auto'>
+                        EDCH54
+                    </button>
+
+                    <button className='my-wallet p-3  float-right'>
+                        <span className='text-[#800080]'>
+                            Wallet Balence
+                        </span>
+                        <p className='text-start px-2'> ₹ 500</p>
+                    </button>
+
+                </div>
+                <div className='py-[79px]'>
+                    <h3 className='friends-enrolled mb-[20px]'> Friends who enrolled
+                        <span className='text-[#626262] px-[1px]'>
+                            (3) </span>
+                    </h3>
+                    <div className='box flex md:flex-row flex-col pb-[20px] text-[#FFFFFF]'>
+                        <div className='curd '>
+                            <h4 className='curd-h4 flex justify-between px-[20px] py-2'>
+                                <span className='font-bold'>Dhiraj Saxsena</span>
+                                <span className='font-normal'>14 sep, 2022</span></h4>
+                            <p className='text-[14px] px-[20px]'> Courses Enrolled(6)</p>
+                            <div className='course-name flex flex-wrap px-[30px] mt-[14px]'>
+
+                                <button className='mx-[3px] my-[5px] py-[4px] text-[14px] rounded-full'> <span className='px-[8px] '> UI/UX </span></button>
+                                <button className='mx-[3px] my-[5px] py-[4px] text-[14px] rounded-full'> <span className='px-[8px] '> Photoshop </span></button>
+                                <button className='mx-[3px] my-[5px] py-[4px] text-[14px] rounded-full'> <span className='px-[8px] '> Illustrator </span></button>
+                                <button className='mx-[3px] my-[5px] py-[4px] text-[14px] rounded-full'> <span className='px-[8px] '> Python </span></button>
+                                <button className='mx-[3px] my-[5px] py-[4px] text-[14px] rounded-full'> <span className='px-[8px] '> MERN </span></button>
+                                <button className='mx-[3px] my-[5px] py-[4px] text-[14px] rounded-full'> <span className='px-[8px] '> Java </span></button>
+
+                            </div>
+                            <h2 className='px-[20px] pt-[3px] flex '>
+                                <p className=' text-[16px] py-1'>Referral Amount</p>
+                                <p className='font-bold text-[24px] px-[10px]'>₹185</p>
+                            </h2>
+                        </div>
+                        <div className='curd'>
+                            <h4 className='curd-h4 flex justify-between px-[20px] py-2'>
+                                <span className='font-bold'>Subhash Mishra</span>
+                                <span className='font-normal'>15 sep, 2022</span></h4>
+                            <p className='text-[14px] px-[20px]'> Courses Enrolled(23)</p>
+                            <div className='course-name flex flex-wrap px-[30px] mt-[14px]'>
+
+                                <button className='mx-[3px] my-[5px] py-[4px] text-[14px] rounded-full'> <span className='px-[8px] '> UI/UX </span></button>
+                                <button className='mx-[3px] my-[5px] py-[4px] text-[14px] rounded-full'> <span className='px-[8px] '> Photoshop </span></button>
+                                <button className='mx-[3px] my-[5px] py-[4px] text-[14px] rounded-full'> <span className='px-[8px] '> Illustrator </span></button>
+                                <button className='mx-[3px] my-[5px] py-[4px] text-[14px] rounded-full'> <span className='px-[8px] '> Python </span></button>
+                                <button className='mx-[3px] my-[5px] py-[4px] text-[14px] rounded-full'> <span className='px-[8px] '> MERN </span></button>
+                                <button className='mx-[3px] my-[5px] py-[4px] text-[14px] rounded-full'> <span className='px-[8px] '> Java </span></button>
+                                <button className='mx-[3px] my-[5px] py-[4px] text-[14px] rounded-full'> <span className='px-[8px] '> C++ </span></button>
+
+                            </div>
+                            <h2 className='px-[20px] pt-[3px] flex '>
+                                <p className=' text-[16px] py-1'>Referral Amount</p>
+                                <p className='font-bold text-[24px] px-[10px]'>₹485</p>
+                            </h2>
+                        </div>
+                        <div className=' curd'>
+                            <h4 className='curd-h4 flex justify-between px-[20px] py-2'>
+                                <span className='font-bold'>Prafull Kumar</span>
+                                <span className='font-normal'>14 sep, 2022</span></h4>
+                            <p className='text-[14px] px-[20px]'> Courses Enrolled(6)</p>
+                            <div className='course-name flex flex-wrap px-[30px] mt-[14px]'>
+
+                                <button className='mx-[3px] my-[5px] py-[4px] text-[14px] rounded-full'> <span className='px-[8px] '> UI/UX </span></button>
+                                <button className='mx-[3px] my-[5px] py-[4px] text-[14px] rounded-full'> <span className='px-[8px] '> Photoshop </span></button>
+                                <button className='mx-[3px] my-[5px] py-[4px] text-[14px] rounded-full'> <span className='px-[8px] '> Illustrator </span></button>
+                                <button className='mx-[3px] my-[5px] py-[4px] text-[14px] rounded-full'> <span className='px-[8px] '> Python </span></button>
+                                <button className='mx-[3px] my-[5px] py-[4px] text-[14px] rounded-full'> <span className='px-[8px] '> MERN </span></button>
+                                <button className='mx-[3px] my-[5px] py-[4px] text-[14px] rounded-full'> <span className='px-[8px] '> Java </span></button>
+
+                            </div>
+                            <h2 className='px-[20px] pt-[3px] flex '>
+                                <p className=' text-[16px] py-1'>Referral Amount</p>
+                                <p className='font-bold text-[24px] px-[10px]'>₹185</p>
+                            </h2>
+                        </div>
+                    </div>
+                    <h2 className='text-[#800080] text-[16px] pt-[60px]'>Terms & Conditions</h2>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/component/Navigation/Navigation.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import logo1 from '../../img/image.png'
+import './Navigation.css'
+const Navigation = () => {
+    return (
+        <div>
+
+            <header className="px-[74px] py-[37px] w-[1366px] h-[73.03px] m-auto bg-[#FFFFFF] ">
+                <div className="container flex justify-between h-16 mx-auto">
+                    <a rel="noopener noreferrer" href="#" aria-label="Back to homepage" className="flex items-center p-2">
+                        <img className='w-[160px] h-[63px]' src={logo1} alt="" />
+                    </a>
+                    <ul className="items-stretch hidden space-x-3 md:flex">
+                        <li className="flex">
+                            <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 my-color ">My Assignment</a>
+                        </li>
+                        <li className="flex">
+                            <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 my-color ">Chat with modarator</a>
+                        </li>
+                        <li className="flex">
+                            <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 text-[#800080] text-[18px]">
+                                <span className='profile-position pt-[1px] px-[6px]'>
+
+                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM10 3C11.66 3 13 4.34 13 6C13 7.66 11.66 9 10 9C8.34 9 7 7.66 7 6C7 4.34 8.34 3 10 3ZM10 17.2C7.5 17.2 5.29 15.92 4 13.98C4.03 11.99 8 10.9 10 10.9C11.99 10.9 15.97 11.99 16 13.98C14.71 15.92 12.5 17.2 10 17.2Z" fill="#800080" />
+                                    </svg>
+                                </span>
+
+                                ProfileName
+                                <span className='px-[10px] mt-1'>
+                                    <svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M1.41 0.589844L6 5.16984L10.59 0.589844L12 1.99984L6 7.99984L0 1.99984L1.41 0.589844Z" fill="#800080" />
+                                    </svg>
+
+                                </span>
+                            </a>
+                        </li>
+
+                    </ul>
+                    <button className="flex justify-end p-4 md:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                        </svg>
+                    </button>
+                </div>
+            </header>
+        </div>
+    );
+};
+
+export default Navigation;
